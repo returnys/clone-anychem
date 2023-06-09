0d24bac8b2e8a37230cd8df3740d0be179a1ec73
returnys <return.ys@gmail.com>
2023-06-09 18:04:02 +0900

    feat: news 영역 json 연동 및 반응형 완료, info 영역 작업중

---
+++ js/news.js
@@ -0,0 +1,63 @@
+window.addEventListener("load", function () {
+  // 외부 데이터 로딩
+  //   fetch("data/newsdata.json")
+  //     .then((res) => res.json)
+  //     .then((result) => console.log(result))
+  //     .catch((err) => console.log(err));
+  const getNews = async () => {
+    try {
+      const res = await fetch("data/newsdata.json");
+      const result = await res.json();
+      makeHtml(result);
+    } catch {
+      console.log(err);
+    }
+  };
+  let makeHtml = (_data) => {
+    let html = ``;
+    _data.forEach((item) => {
+      let tag = `
+        <div class="swiper-slide">
+            <div class="news-box">
+                <a href="${item.link}">
+                    <span class="news-cate">${item.cate}</span>
+                    <p>${item.title}</p>
+                    <span class="news-date">
+                        <i class="fa-regular fa-clock"></i>
+                        ${item.date}
+                    </span>
+                </a>
+            </div>
+        </div>
+        `;
+      html += tag;
+    });
+    const newsWrap = document.querySelector(".sw-news .swiper-wrapper");
+    newsWrap.innerHTML = html;
+    // swiper 생성
+    new Swiper(".sw-news", {
+      slidesPerView: 4,
+      spaceBetween: 20,
+      loop: true,
+      autoplay: {
+        delay: 2500,
+        speed: 800,
+      },
+      breakpoints: {
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+      },
+      pagination: {
+        el: ".sw-news-pg",
+        clickable: true,
+      },
+    });
+  };
+  getNews();
+});
